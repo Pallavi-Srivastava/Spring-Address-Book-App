c0371d446a8e81e2917de7449cbe6cc4d0f4c88c
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-23 21:58:09 +0530

    Added:Code to Use Lombok Library for Logging.

---
+++ src/main/java/com/blz/example/dto/AddressBookDTO.java
@@ -0,0 +1,18 @@
+package com.blz.example.dto;
+
+public class AddressBookDTO {
+
+	public String name;
+	public String address;
+
+	public AddressBookDTO(String name, String address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBookDTO [name=" + name + ", address=" + address + "]";
+	}
+}
