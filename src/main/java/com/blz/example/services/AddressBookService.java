cfae86963e5278693b408daa84e99e3c9f311cf7
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-23 22:15:54 +0530

    Added:Code to determine Logging Levels,based on the application running in dev or production.

---
+++ src/main/java/com/blz/example/services/AddressBookService.java
@@ -0,0 +1,45 @@
+package com.blz.example.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.blz.example.dto.AddressBookDTO;
+import com.blz.example.model.AddressBookData;
+
+@Service
+public class AddressBookService implements IAddressBookService {
+
+	private List<AddressBookData> addressBookList = new ArrayList<>();
+
+	@Override
+	public List<AddressBookData> getAddressBookData() {
+		return addressBookList;
+	}
+
+	@Override
+	public AddressBookData getAddressBookDataById(int personId) {
+		return addressBookList.get(personId - 1);
+	}
+
+	@Override
+	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = null;
+		addressBookData = new AddressBookData(addressBookList.size() + 1, addressBookDTO);
+		addressBookList.add(addressBookData);
+		return addressBookData;
+	}
+
+	@Override
+	public AddressBookData updateAddressBookData(int personId, AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = this.getAddressBookDataById(personId);
+		addressBookData.setName(addressBookDTO.name);
+		addressBookData.setAddress(addressBookDTO.address);
+		addressBookList.set(personId - 1, addressBookData);
+		return addressBookData;
+	}
+
+	@Override
+	public void deleteAddressBookData(int personId) {
+		addressBookList.remove(personId - 1);
+	}
+}
