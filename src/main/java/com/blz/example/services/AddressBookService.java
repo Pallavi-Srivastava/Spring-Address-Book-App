4fd521e4008940b5592f6ba9baa6f1c8340eff03
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-23 15:41:51 +0530

    Added:Code to Introducing Service Layer in Address Book App.

---
+++ src/main/java/com/blz/example/services/AddressBookService.java
@@ -0,0 +1,42 @@
+package com.blz.example.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.blz.example.dto.AddressBookDTO;
+import com.blz.example.model.AddressBookData;
+
+@Service
+public class AddressBookService implements IAddressBookService {
+
+	@Override
+	public List<AddressBookData> getAddressBookData() {
+		List<AddressBookData> addressBookList=new ArrayList<AddressBookData>();
+		addressBookList.add(new AddressBookData(1, new AddressBookDTO("Pallavi", "Btm")));
+		return addressBookList;
+	}
+
+	@Override
+	public AddressBookData getAddressBookDataById(int personId) {
+		AddressBookData addressBookData=new AddressBookData(personId, new AddressBookDTO("Jiya", "JP Nagar"));
+		return addressBookData;
+	}
+
+	@Override
+	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData=new AddressBookData(3, addressBookDTO);
+		return addressBookData;
+	}
+
+	@Override
+	public AddressBookData updateAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData=new AddressBookData(3,addressBookDTO);
+		return addressBookData;
+	}
+
+	@Override
+	public void deleteAddressBookData(int personId) {
+		// TODO Auto-generated method stub
+		
+	}
+}
