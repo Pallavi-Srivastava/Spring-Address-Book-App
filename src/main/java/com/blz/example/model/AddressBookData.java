23b32a62c36b9ba4f185a725a40631ca1205de0c
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-28 01:45:59 +0530

    Added:Code to ensure validation done on every field of address book.

---
+++ src/main/java/com/blz/example/model/AddressBookData.java
@@ -0,0 +1,30 @@
+package com.blz.example.model;
+
+import com.blz.example.dto.AddressBookDTO;
+import lombok.Data;
+
+@Data
+public class AddressBookData {
+
+	private int addressBookId;
+	private String name;
+	private String address;
+	public String phoneNumber;
+	public String city;
+	public String state;
+	public String zipCode;
+
+	public AddressBookData(int addressBookId, AddressBookDTO addressBookDTO) {
+		this.addressBookId = addressBookId;
+		this.updateAddressBookData(addressBookDTO);
+	}
+
+	public void updateAddressBookData(AddressBookDTO addressBookDTO) {
+		this.name = addressBookDTO.name;
+		this.address = addressBookDTO.address;
+		this.phoneNumber = addressBookDTO.phoneNumber;
+		this.city = addressBookDTO.city;
+		this.state = addressBookDTO.state;
+		this.zipCode = addressBookDTO.zipCode;
+	}
+}
