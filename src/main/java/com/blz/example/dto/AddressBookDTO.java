4dae46c4d403cda0813547671f53de558719947e
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-28 02:22:23 +0530

    Added:Code to save address book data to MySql DB.

---
+++ src/main/java/com/blz/example/dto/AddressBookDTO.java
@@ -0,0 +1,31 @@
+package com.blz.example.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import lombok.ToString;
+
+@ToString
+public class AddressBookDTO {
+
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Name is Invalid")
+	@NotEmpty(message = "Name cannot be null ")
+	public String name;
+
+	@Pattern(regexp = "^([A-Za-z0-9/,-]{3,}[ ]?)+$", message = "Address is Invalid")
+	@NotEmpty(message = "Address cannot be null")
+	public String address;
+
+	@Pattern(regexp = "^[9][1][ ][6-9][0-9]{9}$", message = "PhoneNo is Invalid")
+	@NotEmpty(message = "PhoneNo cannot be null")
+	public String phoneNumber;
+
+	@NotEmpty(message = "City cannot be null")
+	public String city;
+
+	@NotEmpty(message = "State cannot be null")
+	public String state;
+
+	@NotEmpty(message = "zipCode cannot be null")
+	public String zipCode;
+
+}
---
+++ src/main/java/com/blz/example/services/AddressBookService.java
@@ -0,0 +1,57 @@
+package com.blz.example.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.blz.example.dto.AddressBookDTO;
+import com.blz.example.exceptions.AddressBookException;
+import com.blz.example.model.AddressBookData;
+import com.blz.example.repository.AddressBookRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class AddressBookService implements IAddressBookService {
+	
+	@Autowired
+	private AddressBookRepository addressBookRepository;
+
+	private List<AddressBookData> addressBookList = new ArrayList<>();
+
+	@Override
+	public List<AddressBookData> getAddressBookData() {
+		return addressBookList;
+	}
+
+	@Override
+	public AddressBookData getAddressBookDataById(int personId) {
+		return addressBookList.stream().filter(list -> list.getAddressBookId() == personId).findFirst()
+				.orElseThrow(() -> new AddressBookException("Record not present"));
+	}
+
+	@Override
+	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = null;
+		addressBookData = new AddressBookData(addressBookDTO);
+		addressBookList.add(addressBookData);
+		log.debug("AddressBook data : "+addressBookData.toString());
+		return addressBookRepository.save(addressBookData);
+	}
+
+	@Override
+	public AddressBookData updateAddressBookData(int personId, AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = this.getAddressBookDataById(personId);
+		addressBookData.setName(addressBookDTO.name);
+		addressBookData.setAddress(addressBookDTO.address);
+		addressBookList.set(personId - 1, addressBookData);
+		return addressBookData;
+	}
+
+	@Override
+	public void deleteAddressBookData(int personId) {
+		addressBookList.remove(personId - 1);
+	}
+}
